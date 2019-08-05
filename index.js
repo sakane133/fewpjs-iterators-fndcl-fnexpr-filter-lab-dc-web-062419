063c109273739d43e85513f01ba3ca7f6e58b698
Samantha Kane <s.kane133@gmail.com>
2019-08-05 21:40:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 
 function findMatching(arr, string){
-  arr.filter(str.downcase => {
-    return str === string.downcase
+  return arr.filter(str.downcase => {
+    return  str === string.downcase
   })
   
 }
